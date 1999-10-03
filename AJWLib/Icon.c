da6913a50cb1f906ee2d7e2d37078057030baffa
Alex Waugh <alex@alexwaugh.com>
1999-10-03 00:03:41 +0000

    Modified to use Desk

--- AJWLib/Icon.c
+++ AJWLib/Icon.c
@@ -3,43 +3,50 @@
 	©Alex Waugh 1998
 
 	$Log: not supported by cvs2svn $
+	Revision 1.1  1999/10/02 23:06:48  AJW
+	Initial revision
+
 
 */
-#include "DeskLib:WimpSWIs.h"
-#include "DeskLib:KernelSWIs.h"
-#include "DeskLib:SWI.h"
-#include "DeskLib:Icon.h"
-#include "DeskLib:Event.h"
+#include "Desk.DeskMem.h"
+#include "Desk.KernelSWIs.h"
+#include "Desk.SWI.h"
+#include "Desk.Icon.h"
+#include "Desk.Event.h"
+#include "Desk.Error2.h"
 
 #include <stdlib.h>
 #include <string.h>
 
-static BOOL Icon_CheckAdjust(event_pollblock *block,void *r)
+#define SWI_Wimp_SpriteOp 0x400E9
+#define SWI_Wimp_CreateIcon 0x400C2
+
+static Desk_bool Icon_CheckAdjust(Desk_event_pollblock *block,void *r)
 {
-	if (!block->data.mouse.button.data.adjust) return FALSE;
-	if (!Icon_GetSelect(block->data.mouse.window,block->data.mouse.icon)) Icon_Select(block->data.mouse.window,block->data.mouse.icon);
-	return FALSE;
+	if (!block->data.mouse.button.data.adjust) return Desk_FALSE;
+	if (!Desk_Icon_GetSelect(block->data.mouse.window,block->data.mouse.icon)) Desk_Icon_Select(block->data.mouse.window,block->data.mouse.icon);
+	return Desk_FALSE;
 }
 
-void Icon_RegisterCheckAdjust(window_handle win,icon_handle icon)
+void Icon_RegisterCheckAdjust(Desk_window_handle win,Desk_icon_handle icon)
 {
-	Event_Claim(event_CLICK,win,icon,Icon_CheckAdjust,NULL);
+	Desk_Error2_CheckBOOL(Desk_Event_Claim(Desk_event_CLICK,win,icon,Icon_CheckAdjust,NULL));
 }
 
-icon_handle Icon_FullBarIcon(char *text,char *sprite,int position,int priority)
+Desk_icon_handle Icon_FullBarIcon(char *text,char *sprite,int position,int priority)
 {
-	icon_handle handle;
-	icon_createblock iconblk;
+	Desk_icon_handle handle;
+	Desk_icon_createblock iconblk;
 	int width,height;
 	int factor,textlen=strlen(text);
 	int mode;
-	char *validation=malloc(14);
-    iconblk.window=window_ICONBAR;
-	SWI(3,7,0x400E9,40,NULL,sprite,NULL,NULL,NULL,&width,&height,NULL,&mode); /*Wimp_SpriteOp*/
-    SWI(2,3,SWI_OS_ReadModeVariable,mode,4,NULL,NULL,&factor);
+	char *validation=Desk_DeskMem_Malloc(14);
+    iconblk.window=Desk_window_ICONBAR;
+	Desk_Error2_CheckOS(Desk_SWI(3,7,SWI_Wimp_SpriteOp,40,NULL,sprite,NULL,NULL,NULL,&width,&height,NULL,&mode));
+    Desk_Error2_CheckOS(Desk_SWI(2,3,Desk_SWI_OS_ReadModeVariable,mode,4,NULL,NULL,&factor));
     width<<=factor;
     if (textlen*16>width) width=textlen*16;
-    SWI(2,3,SWI_OS_ReadModeVariable,mode,5,NULL,NULL,&factor);
+    Desk_Error2_CheckOS(Desk_SWI(2,3,Desk_SWI_OS_ReadModeVariable,mode,5,NULL,NULL,&factor));
     height<<=factor;
     if (*text!='\0') {
     	validation[0]='S';
@@ -62,7 +69,7 @@ icon_handle Icon_FullBarIcon(char *text,char *sprite,int position,int priority)
 		iconblk.icondata.flags.value=0x00003002;
 		strcpy(iconblk.icondata.data.spritename,sprite);
     }
-	SWI(2,1,0x400c2,priority,&iconblk,&handle); /*Wimp_CreateIcon*/
+	Desk_Error2_CheckOS(Desk_SWI(2,1,SWI_Wimp_CreateIcon,priority,&iconblk,&handle));
 	return handle;
 }
 
