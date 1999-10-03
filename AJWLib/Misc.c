e281abd2321838cb7909c5ecf9e983ee4f478192
Alex Waugh <alex@alexwaugh.com>
1999-10-03 00:23:59 +0000

    Disabled ReturnMessage

--- AJWLib/Misc.c
+++ AJWLib/Misc.c
@@ -3,10 +3,13 @@
 	© Alex Waugh 1998
 
 	$Log: not supported by cvs2svn $
+	Revision 1.1  1999/10/02 23:07:59  AJW
+	Initial revision
+
 
 */
-#include "DeskLib:WimpSWIs.h"
-#include "DeskLib:Error.h"
+#include "Desk.WimpSWIs.h"
+#include "Desk.Error2.h"
 
 #include <stdio.h>
 
@@ -15,14 +18,14 @@ void Beep(void)
 	putchar('\7');
 }
 
-void ReturnMessage(event_pollblock *block)
+/*void ReturnMessage(event_pollblock *block)
 {
 	switch (block->data.message.header.action) {
-		case message_PREQUIT: /*Reply by sending an acknowledgement*/
+		case message_PREQUIT: Reply by sending an acknowledgement
 			block->data.message.header.yourref=block->data.message.header.myref;
 			Wimp_SendMessage(event_USERMESSAGEACK,&(block->data.message),block->data.message.header.sender,0);
 			break;
-		case message_DATALOAD: /*Reply by sending a dataloadack*/
+		case message_DATALOAD: Reply by sending a dataloadack
 		case message_DATAOPEN:
 			block->data.message.header.action=message_DATALOADACK;
 			block->data.message.header.yourref=block->data.message.header.myref;
@@ -32,3 +35,4 @@ void ReturnMessage(event_pollblock *block)
 			Error_ReportInternal(1,"Unable to reply to message (unknown message type: %d)",block->data.message.header.action);
 	}
 }
+*/
