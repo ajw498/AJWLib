4699f7820c18a4d52eaa94e9fdfbd5cf9280a558
Alex Waugh <alex@alexwaugh.com>
1999-10-04 15:33:56 +0000

    Added semi colons at end of lines!

--- AJWLib/TaskWindow.h
+++ AJWLib/TaskWindow.h
@@ -3,20 +3,23 @@
 	©Alex Waugh 1998
 
 	$Log: not supported by cvs2svn $
+	Revision 1.1  1999/10/04 15:19:43  AJW
+	Initial revision
+
 	
 */
 
 
-void TaskWindow_BringToFront(void)
+void TaskWindow_BringToFront(void);
 
-void TaskWindow_Suspend(void)
+void TaskWindow_Suspend(void);
 
-void TaskWindow_Resume(void)
+void TaskWindow_Resume(void);
 
-void TaskWindow_Close(void)
+void TaskWindow_Close(void);
 
-void TaskWindow_Kill(void)
+void TaskWindow_Kill(void);
 
-void TaskWindow_Start(char *commandtext,char *message,void (*fn)(BOOL))
+void TaskWindow_Start(char *commandtext,char *message,void (*fn)(BOOL));
 
 
