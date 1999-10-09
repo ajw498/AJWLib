fac039e0e038d163dce84a6fda94d303f602be5f
Alex Waugh <alex@alexwaugh.com>
1999-10-09 17:35:59 +0000

    Added AJWLib_ prefix

--- AJWLib/FilerActn.c
+++ AJWLib/FilerActn.c
@@ -3,6 +3,9 @@
 	©Alex Waugh 1998
 
 	$Log: not supported by cvs2svn $
+	Revision 1.3  1999/10/03 00:48:08  AJW
+	Removed Macros
+
 	Revision 1.2  1999/10/02 23:40:03  AJW
 	Modified to use Desk
 
@@ -18,18 +21,21 @@
 
 #include <string.h>
 
-#define options_VERBOSE 1
-#define options_CONFIRM 2
-#define options_FORCE   4
-#define options_NEWER   8
-#define options_RECURSE 16
+#define AJWLib_options_VERBOSE 1
+#define AJWLib_options_CONFIRM 2
+#define AJWLib_options_FORCE   4
+#define AJWLib_options_NEWER   8
+#define AJWLib_options_RECURSE 16
 
-void FilerAction_Start(char *source,char *destdir,int options,int type)
+void AJWLib_FilerAction_Start(char *source,char *destdir,int options,int type)
 {
 	Desk_task_handle task;
 	Desk_message_block msg;
 	Desk_Wimp_StartTask3("Filer_Action",&task);
-	if (task==0) return;
+	if (task==0) {
+		Desk_Error2_HandleText("FilerAction failed to start");
+		return;
+	}
 	msg.header.size=256;
 	msg.header.yourref=0;
 	msg.header.action=Desk_message_FILERSELECTIONDIRECTORY;
