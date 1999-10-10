24525420dbd745943642126ec1ee8bb368cef4ff
Alex Waugh <alex@alexwaugh.com>
1999-10-10 14:01:41 +0000

    Now works

--- AJWLib/Error2.h
+++ AJWLib/Error2.h
@@ -1,4 +1,6 @@
 
-void AJWLib_Error2_Report(const Desk_error2_block* error);
+Desk_error2_block *AJWLib_Error2_ReportFatal(Desk_error2_block* error);
 
-void AJWLib_Error2_ReportFatal(const Desk_error2_block* error);
+Desk_error2_block *AJWLib_Error2_Report(Desk_error2_block* error);
+
+char* AJWLib_Error2_Describe(Desk_error2_block* error);
