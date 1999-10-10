96d4d4acfcfb555f840648d49daf0001d2e64b68
Alex Waugh <alex@alexwaugh.com>
1999-10-10 14:46:08 +0000

    Modified to use Desk

--- AJWLib/TaskWindow.h
+++ AJWLib/TaskWindow.h
@@ -3,6 +3,9 @@
 	©Alex Waugh 1998
 
 	$Log: not supported by cvs2svn $
+	Revision 1.3  1999/10/10 14:43:41  AJW
+	Added AJWLib_ prefix
+
 	Revision 1.2  1999/10/04 15:33:56  AJW
 	Added semi colons at end of lines!
 
@@ -23,6 +26,6 @@ void AJWLib_TaskWindow_Close(void);
 
 void AJWLib_TaskWindow_Kill(void);
 
-void AJWLib_TaskWindow_Start(char *commandtext,char *message,void (*fn)(BOOL));
+void AJWLib_TaskWindow_Start(char *commandtext,char *message,void (*fn)(Desk_bool));
 
 
