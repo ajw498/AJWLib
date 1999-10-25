0bf2827e18b7d33714c0b113b80472d10bc5cb50
Alex Waugh <alex@alexwaugh.com>
1999-10-25 17:23:12 +0000

    Added #includes and #defines so it actually compiled

--- AJWLib/Font.c
+++ AJWLib/Font.c
@@ -3,9 +3,18 @@
 	© Alex Waugh 1999
 
 	$Log: not supported by cvs2svn $
+	Revision 1.1  1999/10/25 17:17:02  AJW
+	Initial revision
+
 
 */
 
+#include "Desk.Font.h"
+#include "Desk.Error2.h"
+#include "Desk.SWI.h"
+
+#define SWI_Font_ScanString 0x400A1
+#define INFINITY 999999
 
 int AJWLib_Font_GetWidth(Desk_font_handle handle,const char *str)
 {
