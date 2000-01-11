2a2f73e335164ce48a07656dd9414d61ef401033
Alex Waugh <alex@alexwaugh.com>
2000-01-11 17:03:14 +0000

    Added semicolons at end of lines!

--- AJWLib/File.h
+++ AJWLib/File.h
@@ -1,8 +1,8 @@
 
-FILE *AJWLib_File_fopen(const char *filename,const char *mode)
+FILE *AJWLib_File_fopen(const char *filename,const char *mode);
 
-void AJWLib_File_fread(void *ptr,size_t size,size_t nobj,FILE *stream)
+void AJWLib_File_fread(void *ptr,size_t size,size_t nobj,FILE *stream);
 
-void AJWLib_File_fwrite(void *ptr,size_t size,size_t nobj,FILE *stream)
+void AJWLib_File_fwrite(void *ptr,size_t size,size_t nobj,FILE *stream);
 
-void AJWLib_File_fclose(FILE *stream)
+void AJWLib_File_fclose(FILE *stream);
