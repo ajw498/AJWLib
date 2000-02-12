9cfb0e6aadb8e9ee6f97722d6dc6b5bc57dd7d88
Alex Waugh <alex@alexwaugh.com>
2000-02-12 23:55:59 +0000

    ?

--- AJWLib/TaskWindow.h
+++ AJWLib/TaskWindow.h
@@ -1,20 +1,3 @@
-/*
-	TaskWindow
-	©Alex Waugh 1998
-
-	$Log: not supported by cvs2svn $
-	Revision 1.3  1999/10/10 14:43:41  AJW
-	Added AJWLib_ prefix
-
-	Revision 1.2  1999/10/04 15:33:56  AJW
-	Added semi colons at end of lines!
-
-	Revision 1.1  1999/10/04 15:19:43  AJW
-	Initial revision
-
-	
-*/
-
 
 void AJWLib_TaskWindow_BringToFront(void);
 
