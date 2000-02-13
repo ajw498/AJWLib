b3f2689dd3149f6df6884425f6246b2dbeb762db
Alex Waugh <alex@alexwaugh.com>
2000-02-13 00:09:39 +0000

    No longer needed

--- AJWLib/Handler.h
+++
@@ -1,1 +0,0 @@
-extern Desk_bool AJWLib_Handler_KeyPress(Desk_event_pollblock *block,void *r);
--- AJWLib/Misc.h
+++
@@ -1,5 +0,0 @@
-
-extern void Beep(void);
-
-/*extern void ReturnMessage(Desk_event_pollblock *block);
-*/
