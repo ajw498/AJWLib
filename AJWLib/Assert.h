8153acca1ed7c8cea7d7758e93b8038b9854224e
Alex Waugh <alex@alexwaugh.com>
2001-06-08 19:27:13 +0000

    Added printing of the actual assertion.

--- AJWLib/Assert.h
+++ AJWLib/Assert.h
@@ -9,19 +9,19 @@
 #else
 	#ifdef AJWLIB_ASSERT_ERROR2
 	
-		#define AJWLib_Assert(x) if (!(x)) Desk_Error2_HandleText("Assertion failed. "Desk_error_PLACE);
+		#define AJWLib_Assert(x) if (!(x)) Desk_Error2_HandleText("Assertion failed. (" #x ") "Desk_error_PLACE);
 		
 		#define AJWLib_AssertWarning(x)
 	
 	#else
 	
 		#define AJWLib_Assert(x) if (!(x)) { \
-			Desk_Error_Report(1,"Assertion failed. "Desk_error_PLACE);\
+			Desk_Error_Report(1,"Assertion failed. (" #x ") "Desk_error_PLACE);\
 			exit(1);\
 		}
 		
 		#define AJWLib_AssertWarning(x) if (!(x)) { \
-			Desk_Error_Report(1,"Assertion warning. "Desk_error_PLACE);\
+			Desk_Error_Report(1,"Assertion warning. (" #x ") "Desk_error_PLACE);\
 		}
 	
 	#endif
